0984dc2aafc129530dea093e4895c70a3cdd378c
yoyotime <dev@yoyotime.app>
2026-06-20 19:49:20 +0800

    fix(domain): fix runtimeType access in DomainEvent initializer

--- lib/domain/event/domain_event.dart
+++ lib/domain/event/domain_event.dart
@@ -1,10 +1,10 @@
 abstract class DomainEvent {
   final DateTime occurredAt;
-  final String eventName;
+  late final String eventName;
 
-  DomainEvent({String? eventName})
-      : occurredAt = DateTime.now(),
-        eventName = eventName ?? runtimeType.toString();
+  DomainEvent({String? eventName}) : occurredAt = DateTime.now() {
+    this.eventName = eventName ?? runtimeType.toString();
+  }
 
   @override
   String toString() => '$eventName(${occurredAt.millisecondsSinceEpoch})';
